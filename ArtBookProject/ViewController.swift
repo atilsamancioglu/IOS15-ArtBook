5a2897a0621f3c0fced45c50bfd1092ef02cf42f
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-07-26 10:50:20 +0300

    Initial Commit

---
+++ ArtBookProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtBookProject
+//
+//  Created by Atil Samancioglu on 26.07.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
